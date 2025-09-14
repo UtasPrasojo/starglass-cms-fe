9c501ead4370c4ae563b0e682255d9cf04a3c5f7
Utas Prasojo <utasprasojo18@student.uns.ac.id>
2025-09-14 13:32:10 +0700

    feat:adjust tailwind v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const animate = require('tailwindcss-animate');
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,7 +8,18 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: 'var(--card)',
+        primary: 'var(--primary)',
+      },
+      borderRadius: {
+        DEFAULT: 'var(--radius)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [animate],
+  darkMode: 'class',
 };
